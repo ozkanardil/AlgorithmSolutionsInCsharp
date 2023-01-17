d6cbc5a4339bacb5d595ddade3886d8d0f7d0976
Ozkan Ardil <ozkanardil@gmail.com>
2023-01-17 22:44:49 +0300

    ContainDublicate has been added

---
+++ src/ContainsDublicate/Code/SolutionOne.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainsDublicate.Code
+{
+    public class SolutionOne
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            bool result = false;
+            HashSet<int> mySet = new HashSet<int>();
+            foreach (int item in nums)
+            {
+                if (mySet.Contains(item))
+                {
+                    return true;
+                }
+                else
+                {
+                    mySet.Add(item);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ src/ContainsDublicate/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ContainsDublicate.Code;
+
+Console.WriteLine("Hello, World!");
+
+SolutionOne myClass = new SolutionOne();
+int[] nums = new int[] { 1, 2, 3, 1 };
+var result = myClass.ContainsDuplicate(nums);
+Console.WriteLine(result);
