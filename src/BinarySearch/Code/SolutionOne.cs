54123a567cd719a11e1adf077613d174e48d4070
Ozkan Ardil <ozkanardil@gmail.com>
2023-01-18 22:11:58 +0300

    Binary search has been added to the soluiton

---
+++ src/BinarySearch/Code/SolutionOne.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch.Code
+{
+    public class SolutionOne
+    {
+        public int Search(int[] nums, int target)
+        {
+            int low = 0;
+            int high = nums.Length - 1;
+            int mid;
+
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+
+                if (nums[mid] == target)
+                    return mid;
+
+                if (target < nums[mid])
+                    high = mid - 1;
+
+                if (target > nums[mid])
+                    low = mid + 1;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ src/BinarySearch/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BinarySearch.Code;
+
+Console.WriteLine("Binary search!");
+
+SolutionOne myClass = new SolutionOne();
+int[] nums = new int[] { -1, 0, 3, 5, 9, 12 };
+int target = 2;
+var result = myClass.Search(nums, target);
+Console.WriteLine(result);
