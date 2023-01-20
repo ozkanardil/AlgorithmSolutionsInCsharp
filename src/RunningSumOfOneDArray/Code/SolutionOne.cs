69488a1a5bc062fc05119c956fd7175fca1de641
Ozkan Ardil <ozkanardil@gmail.com>
2023-01-20 20:18:46 +0300

    Running Sum of 1d Array has been solved.

---
+++ src/RunningSumOfOneDArray/Code/SolutionOne.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunningSumOfOneDArray.Code
+{
+    public class SolutionOne
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            int mySum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                mySum = mySum + nums[i];
+                nums[i] = mySum;
+            }
+            return nums;
+        }
+    }
+}
---
+++ src/RunningSumOfOneDArray/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RunningSumOfOneDArray.Code;
+
+Console.WriteLine("Running Sum of 1d Array");
+
+SolutionOne myClass = new SolutionOne();
+int[] nums = new int[] { 1, 2, 3, 4 };
+var result = myClass.RunningSum(nums);
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
