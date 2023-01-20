aa1d6c1981ae82e1806df98735801996f24bd2c6
Ozkan Ardil <ozkanardil@gmail.com>
2023-01-20 20:43:37 +0300

    Find Pivot Index problem has been solved.

---
+++ src/FindPivotIndex/Code/SolutionOne.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindPivotIndex.Code
+{
+    public class SolutionOne
+    {
+        public int PivotIndex(int[] nums)
+        {
+            int totalSum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                totalSum = totalSum + nums[i];
+            }
+
+            int leftSum = 0;
+
+            for (int k = 0; k < nums.Length; k++)
+            {
+                int rightSum = totalSum - nums[k] - leftSum;
+
+                if (rightSum == leftSum)
+                {
+                    return k;
+                }
+
+                leftSum = leftSum + nums[k];
+            }
+            return -1;
+        }
+    }
+}
---
+++ src/FindPivotIndex/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FindPivotIndex.Code;
+
+Console.WriteLine("Find Pivot Index");
+
+SolutionOne myClass = new SolutionOne();
+int[] nums = new int[] { 1, 2, 3 };
+var result = myClass.PivotIndex(nums);
+Console.WriteLine(result);
