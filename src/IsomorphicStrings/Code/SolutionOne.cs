838f74a662653836d55269bf8294202bddfb2056
Ozkan Ardil <ozkanardil@gmail.com>
2023-01-28 15:59:59 +0300

    Isomorphic Strings solution has been added

---
+++ src/IsomorphicStrings/Code/SolutionOne.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsomorphicStrings.Code
+{
+    public class SolutionOne
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+            
+            int counter = 1;
+            var arrS = new int[127];
+            var arrT = new int[127];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (arrS[s[i]] != arrT[t[i]])
+                    return false;
+                else if (arrS[s[i]] == 0)
+                {
+                    arrS[s[i]] = counter;
+                    arrT[t[i]] = counter;
+                    counter++;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ src/IsomorphicStrings/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IsomorphicStrings.Code;
+
+Console.WriteLine("Isomorphic Strings!");
+
+
+SolutionOne myClass = new SolutionOne();
+string str1 = "paper";
+string str2 = "title";
+var result = myClass.IsIsomorphic(str1, str2);
+Console.WriteLine(result);
