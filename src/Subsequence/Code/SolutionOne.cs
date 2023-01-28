f64b46846e764885262c126edb1d0669aa686591
Ozkan Ardil <ozkanardil@gmail.com>
2023-01-28 16:27:50 +0300

    Is Subsequence solution has been added

---
+++ src/Subsequence/Code/SolutionOne.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subsequence.Code
+{
+    public class SolutionOne
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            int pointer = 0;
+            foreach (var item in s)
+            {
+                int exist = t.IndexOf(item, pointer == 0 ? 0 : pointer + 1);
+
+                if (exist == -1)
+                    return false;
+
+                if(pointer == 0) {     
+                    pointer = exist;
+                } else if(exist < pointer)
+                {
+                    return false;
+                }
+                else
+                {
+                    pointer = exist;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ src/Subsequence/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Subsequence.Code;
+
+Console.WriteLine("Hello, World!");
+
+SolutionOne MyClass = new SolutionOne();
+string s = "aaaaaa";
+string t = "bbaaaa";
+var result = MyClass.IsSubsequence(s, t);
+Console.WriteLine(result);
