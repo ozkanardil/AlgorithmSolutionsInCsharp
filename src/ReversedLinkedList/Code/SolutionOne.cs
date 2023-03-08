0b3afe656feef2e1eb81180e8272d17fbd29a07a
Özkan Ardil <ozkanardil@gmail.com>
2023-03-08 21:35:15 +0300

    Reversed Linked List has been added.

---
+++ src/ReversedLinkedList/Code/SolutionOne.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedLinkedList.Code
+{
+    public class SolutionOne
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null)
+                return null;
+
+            if (head.next == null)
+                return head;
+
+            ListNode pointer = null;
+            ListNode follower = null;
+
+            while (head != null)
+            {
+                pointer = head.next;
+                head.next = follower;
+                follower = head;
+                head = pointer;
+            }
+
+            return follower;
+        }
+    }
+}
---
+++ src/ReversedLinkedList/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReversedLinkedList.Code;
+
+Console.WriteLine("Reversed Linked List!");
+
+SolutionOne MyClass = new SolutionOne();
+
+ListNode list1 = new ListNode();
+ListNode list1Temp = list1;
+
+for (int i = 1; i < 6; i++)
+{
+    ListNode item = new ListNode();
+    item.val = i;
+    list1Temp.next = item;
+    list1Temp = list1Temp.next;
+}
+
+var result = MyClass.ReverseList(list1.next);
+string sonuc = "";
+
+while (result != null)
+{
+    sonuc += result.val + " - ";
+    result = result.next;
+}
+
+Console.WriteLine(sonuc);
