51d2b81504aa7de31013f11acac97374fdfc34d1
Özkan Ardil <ozkanardil@gmail.com>
2023-03-08 21:59:46 +0300

    Middle Point of The Sorted List has been added.

---
+++ src/MiddleOfTheLinkedList/Code/SolutionOne.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleOfTheLinkedList.Code
+{
+    public class SolutionOne
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            ListNode tempNode = head;
+            
+            if (head.next == null)
+                return head;
+            
+            int counter = 1;
+
+            while (head.next != null)
+            {
+                head = head.next;
+                counter++;
+            }
+
+            int status = counter % 2;
+            int middleIndex = 0;
+
+            middleIndex = (counter/2)+1;
+
+            int resultCounter = 1;
+            while (resultCounter < middleIndex)
+            {
+                tempNode = tempNode.next;
+                resultCounter++;
+            }
+
+            return tempNode;
+        }
+    }
+}
---
+++ src/MiddleOfTheLinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MiddleOfTheLinkedList.Code;
+
+Console.WriteLine("Middle of the Linked List!");
+
+SolutionOne MyClass = new SolutionOne();
+
+ListNode list1 = new ListNode();
+ListNode list1Temp = list1;
+
+for (int i = 1; i < 7; i++)
+{
+    ListNode item = new ListNode();
+    item.val = i;
+    list1Temp.next = item;
+    list1Temp = list1Temp.next;
+}
+
+var result = MyClass.MiddleNode(list1.next);
+
+Console.WriteLine(result.val);
