5c7c90b693244b1e408d443bf9a5fc29a837f69f
Özkan Ardil <ozkanardil@gmail.com>
2023-03-08 20:17:20 +0300

    Merge Sorted List has been added

---
+++ src/MergeTwoSortedList/Code/SolutionOne.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeTwoSortedList.Code
+{
+    public class SolutionOne
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode result = new ListNode();
+            ListNode temp = result;
+            list1 = list1.next;
+            list2 = list2.next;
+
+            while (list1 != null && list2 != null)
+            {
+                    if (list1.val <= list2.val)
+                    {
+                        temp.next = list1;
+                        temp = temp.next;
+                        list1 = list1.next;
+                    }
+                    else
+                    {
+                        temp.next = list2;
+                        temp = temp.next;
+                        list2 = list2.next;
+                    }
+            }
+
+            while (list1 != null)
+            {
+                temp.next = list1;
+                temp = temp.next;
+                list1 = list1.next;
+            }
+
+            while (list2 != null)
+            {
+                temp.next = list2;
+                temp = temp.next;
+                list2 = list2.next;
+            }
+
+            return result.next;
+
+        }
+    }
+}
---
+++ src/MergeTwoSortedList/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MergeTwoSortedList.Code;
+
+Console.WriteLine("Merge Two Sorted List!");
+
+SolutionOne MyClass = new SolutionOne();
+
+ListNode list1 = new ListNode();
+ListNode list1Temp = list1;
+
+ListNode list1Item1 = new ListNode();
+list1Item1.val = 1;
+list1Temp.next = list1Item1;
+list1Temp = list1Temp.next;
+
+ListNode list1Item2 = new ListNode();
+list1Item2.val = 2;
+list1Temp.next = list1Item2;
+list1Temp = list1Temp.next;
+
+ListNode list1Item3 = new ListNode();
+list1Item3.val = 4;
+list1Temp.next = list1Item3;
+list1Temp = list1Temp.next;
+
+ListNode list2 = new ListNode();
+ListNode list2Temp = list2;
+
+ListNode list2Item1 = new ListNode();
+list2Item1.val = 1;
+list2Temp.next = list2Item1;
+list2Temp = list2Temp.next;
+
+ListNode list2Item2 = new ListNode();
+list2Item2.val = 3;
+list2Temp.next = list2Item2;
+list2Temp = list2Temp.next;
+
+ListNode list2Item3 = new ListNode();
+list2Item3.val = 4;
+list2Temp.next = list2Item3;
+list2Temp = list2Temp.next;
+
+var result = MyClass.MergeTwoLists(list1, list2);
+string sonuc = "";
+
+while(result != null)
+{
+    sonuc += result.val + " - ";
+    result = result.next;
+}
+
+Console.WriteLine(sonuc);
