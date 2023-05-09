2326bec4eeec7bcb27fef59e4c10c7e8db933b65
Özkan Ardil <ozkanardil@gmail.com>
2023-05-09 22:41:01 +0300

    Top K Frequent Elements

---
+++ src/TopKFrequentElements/Codes/SolutionOne.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopKFrequentElements.Codes
+{
+    public class SolutionOne
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            List<int> list = new List<int>();
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool checkDict = dict.ContainsKey(nums[i]);
+                if (!checkDict)
+                {
+                    dict.Add(nums[i], 1);
+                } else
+                {
+                    dict[nums[i]] = dict[nums[i]] + 1;
+                }
+            }
+
+            var pk = new PriorityQueue<int, int>();
+            foreach (KeyValuePair<int, int> item in dict)
+            {
+                pk.Enqueue(item.Key, item.Value);
+                if(pk.Count > k)
+                    pk.Dequeue();
+            }
+
+            int[] result = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                result[i] = pk.Dequeue();
+            }
+
+            return result;
+        }
+    }
+   
+}
---
+++ src/TopKFrequentElements/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using TopKFrequentElements.Codes;
+
+Console.WriteLine("Top K Frequent Elements");
+
+int[] nums = { 1, 1, 1, 2, 2, 3, 3, 3, 4 };
+int k = 2;
+
+SolutionOne myClass = new SolutionOne();
+var result = myClass.TopKFrequent(nums, k);
+StringBuilder StringBuilder = new StringBuilder();
+
+for (int i = 0; i < result.Length; i++)
+{
+    StringBuilder.Append(result[i] + " - ");
+}
+
+Console.WriteLine(StringBuilder.ToString());
