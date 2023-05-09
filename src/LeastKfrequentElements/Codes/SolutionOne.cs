7bbea12b76f5dc241b64764123ef3a1603ad0cad
Özkan Ardil <ozkanardil@gmail.com>
2023-05-09 22:39:42 +0300

    Least K Frequesnt Elements

---
+++ src/LeastKfrequentElements/Codes/SolutionOne.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeastKfrequentElements.Codes
+{
+    public class SolutionOne
+    {
+        public int[] LeastKFrequent(int[] nums, int k)
+        {
+            List<int> list = new List<int>();
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool checkDict = dict.ContainsKey(nums[i]);
+                if (!checkDict)
+                {
+                    dict.Add(nums[i], 1);
+                }
+                else
+                {
+                    dict[nums[i]] = dict[nums[i]] + 1;
+                }
+            }
+
+            var pk = new PriorityQueue<int, int>();
+            foreach (KeyValuePair<int, int> item in dict)
+            {
+                pk.Enqueue(item.Key, item.Value);
+            }
+
+            int[] result = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                result[i] = pk.Dequeue();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeastKfrequentElements/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeastKfrequentElements.Codes;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+int[] nums = { 1, 1, 1, 2, 2, 3, 3, 3, 4 };
+int k = 2;
+
+SolutionOne myClass = new SolutionOne();
+var result = myClass.LeastKFrequent(nums, k);
+StringBuilder stringBuilder = new StringBuilder();
+
+for (int i = 0; i < result.Length; i++)
+{
+    stringBuilder.Append(result[i].ToString() + " - ");
+}
+
+Console.WriteLine(stringBuilder.ToString());
