5c5068bf8e9c6cb469d5ecaf6f2479b7f049ab1c
Özkan Ardil <ozkanardil@gmail.com>
2023-05-09 22:39:55 +0300

    Merge Arrays

---
+++ src/MergeArrays/Code/SolutionOne.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeArrays.Code
+{
+    public class SolutionOne
+    {
+        public int[] MergeArraysIntoOneArray(int[] arr1, int[] arr2)
+        {
+            int[] result = new int[arr1.Length + arr2.Length];
+            int iterateCounter = arr1.Length > arr2.Length ? arr1.Length : arr2.Length;
+            int counter = 0;
+            int resultCounter = 0;
+
+            while (counter < iterateCounter)
+            {
+                if(counter < arr1.Length && counter < arr2.Length)
+                {
+                    if(arr1[counter] < arr2[counter])
+                    {
+                        result[resultCounter] = arr1[counter];
+                        result[resultCounter + 1] = arr2[counter];
+                    } else
+                    {
+                        result[resultCounter] = arr2[counter];
+                        result[resultCounter + 1] = arr1[counter];
+                    }
+                    resultCounter++;
+                } else if(counter >= arr1.Length && counter < arr2.Length)
+                {
+                    result[resultCounter] = arr2[counter];
+                } else
+                {
+                    result[resultCounter] = arr1[counter];
+                }
+                resultCounter++;
+                counter++;
+            }
+            return result;  
+        }
+    }
+}
---
+++ src/MergeArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MergeArrays.Code;
+using System.Text;
+
+Console.WriteLine("Merging!");
+
+int[] arr1 = new int[2] {2, 8};
+int[] arr2 = new int[3] {1, 3, 9};
+
+SolutionOne myClass = new SolutionOne();
+int[] result = myClass.MergeArraysIntoOneArray(arr1, arr2);
+
+StringBuilder stringBuilder = new StringBuilder();
+
+foreach (var item in result)
+{
+    stringBuilder.Append(item + " - ");
+}
+
+Console.WriteLine(stringBuilder.ToString());
