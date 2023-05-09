ab4da5bc709952517e1f4f81d520928e2bfb034e
Özkan Ardil <ozkanardil@gmail.com>
2023-05-09 22:40:10 +0300

    Merge Lists

---
+++ src/MergeLists/Codes/SolutionOne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeLists.Codes
+{
+    public class SolutionOne
+    {
+        public List<int> MergeListsIntoOneList(List<int> list1, List<int> list2)
+        {
+            list1.AddRange(list2);
+            list1.Sort();
+            return list1;
+        }
+    }
+}
---
+++ src/MergeLists/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MergeLists.Codes;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+List<int> list1 = new List<int> { 2, 8 };
+List<int> list2 = new List<int> { 1, 3, 9 };
+
+SolutionOne myClass = new SolutionOne();
+List<int> result = myClass.MergeListsIntoOneList(list1, list2);
+
+StringBuilder stringBuilder = new StringBuilder();
+
+foreach (var item in result)
+{
+    stringBuilder.Append(item + " - ");
+}
+
+Console.WriteLine(stringBuilder.ToString());
